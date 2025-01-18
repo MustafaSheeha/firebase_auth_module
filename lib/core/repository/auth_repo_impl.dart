c90bfd21c5cbbe3d22cb0e88115da8b27b571dae
MustafaSheha <mustafa.mahmoud.sheha@gmail.com>
2025-01-18 17:48:52 +0200

    Add AuthRepo and AuthRepoImpl for authentication management
    Introduce an abstract class `AuthRepo` with methods for login, register, logout, and checking if the user is logged in. Also, add a concrete implementation `AuthRepoImpl` that throws `UnimplementedError` for each method, which will be implemented later.

---
+++ lib/core/repository/auth_repo_impl.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth_module/core/repository/auth_repo.dart';
+
+class AuthRepoImpl implements AuthRepo {
+  @override
+  Future<bool> isLoggedIn() {
+    // TODO: implement isLoggedIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> login(String email, String password) {
+    // TODO: implement login
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> logout() {
+    // TODO: implement logout
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> register(String email, String password) {
+    // TODO: implement register
+    throw UnimplementedError();
+  }
+  
+}
