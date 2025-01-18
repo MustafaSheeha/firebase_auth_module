d4bb3d6486763eab0d475bd151b2a9d159214a8e
MustafaSheha <mustafa.mahmoud.sheha@gmail.com>
2025-01-18 18:30:36 +0200

    Implement authentication methods in AuthRepoImpl
    Implemented the `isLoggedIn`, `login`, `logout`, and `register` methods in `AuthRepoImpl` using the `AuthService`. Added error handling and logging for each method.

--- lib/core/repository/auth_repo_impl.dart
+++ lib/core/repository/auth_repo_impl.dart
@@ -1,28 +1,51 @@
+import 'dart:developer';
+
 import 'package:firebase_auth_module/core/repository/auth_repo.dart';
+import 'package:firebase_auth_module/core/services/auth_service.dart';
 
 class AuthRepoImpl implements AuthRepo {
+  final AuthService _authService;
+  AuthRepoImpl(this._authService);
+
   @override
-  Future<bool> isLoggedIn() {
-    // TODO: implement isLoggedIn
-    throw UnimplementedError();
+  Future<bool> isLoggedIn() async {
+    try {
+      final user = await _authService.authStateChanges.first;
+      return user != null;
+    } catch (e) {
+      log('AuthRepoImpl');
+      log(e.toString());
+      return false;
+    }
   }
 
   @override
-  Future<void> login(String email, String password) {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<void> login(String email, String password) async {
+    try {
+      await _authService.signInWithEmailAndPassword(email, password);
+    } catch (e) {
+      log('AuthRepoImpl');
+      log(e.toString());
+    }
   }
 
   @override
-  Future<void> logout() {
-    // TODO: implement logout
-    throw UnimplementedError();
+  Future<void> logout() async {
+    try {
+      await _authService.signOut();
+    } catch (e) {
+      log('AuthRepoImpl');
+      log(e.toString());
+    }
   }
 
   @override
-  Future<void> register(String email, String password) {
-    // TODO: implement register
-    throw UnimplementedError();
+  Future<void> register(String email, String password) async {
+    try {
+      await _authService.createUserWithEmailAndPassword(email, password);
+    } catch (e) {
+      log('AuthRepoImpl');
+      log(e.toString());
+    }
   }
-  
 }
