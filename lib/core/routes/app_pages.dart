ce6f967386c584c30e433a845d9afabc8ea334df
MustafaSheha <mustafa.mahmoud.sheha@gmail.com>
2025-01-19 13:59:25 +0200

    The `RegisterBinding` class is added to the `AppPages` list to ensure that the necessary dependencies are provided when the `RegisterView` is navigated to.

--- lib/core/routes/app_pages.dart
+++ lib/core/routes/app_pages.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth_module/core/routes/app_routes.dart';
 import 'package:firebase_auth_module/screens/home/view/home_view.dart';
 import 'package:firebase_auth_module/screens/login/view/login_view.dart';
+import 'package:firebase_auth_module/screens/register/bindings/register_binding.dart';
 import 'package:firebase_auth_module/screens/register/view/register_view.dart';
 import 'package:firebase_auth_module/screens/splash/bindings/splash_binding.dart';
 import 'package:firebase_auth_module/screens/splash/view/splash_view.dart';
@@ -23,6 +24,7 @@ abstract class AppPages {
     ),
     GetPage(
       name: AppRoutes.register,
+      binding: RegisterBinding(),
       page: () => RegisterView(),
     ),
   ];
