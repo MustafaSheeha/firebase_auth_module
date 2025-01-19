5c2754f43b0209c53d8e158b66d50b299914b1fd
MustafaSheha <mustafa.mahmoud.sheha@gmail.com>
2025-01-19 12:25:05 +0200

    Extend AuthService to use GetxService and add Get.find for FirebaseAuthService
    Updated AuthService to extend GetxService for better integration with GetX. Added a static getter to FirebaseAuthService to utilize Get.find for easier dependency injection.

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
 
-abstract class AuthService {
+abstract class AuthService extends GetxService {
   Future<void> createUserWithEmailAndPassword(String email, String password);
   Future<void> signInWithEmailAndPassword(String email, String password);
   Future<void> signOut();
--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_module/core/services/auth_service.dart';
+import 'package:get/get.dart';
 
-class FirebaseAuthService implements AuthService {
+class FirebaseAuthService extends AuthService {
+  static FirebaseAuthService get to => Get.find<FirebaseAuthService>();
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   @override
   Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
