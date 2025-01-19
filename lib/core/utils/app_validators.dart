afc9131b7cc1bc73287d0bb1c475c8d04000f7c1
MustafaSheha <mustafa.mahmoud.sheha@gmail.com>
2025-01-19 15:01:04 +0200

    Add email and password validation methods
    Introduce `AppValidators` class with static methods to validate email and password inputs. The `validateEmail` method checks if the input is a valid email address, and the `validatePassword` method ensures the password is at least 6 characters long.

---
+++ lib/core/utils/app_validators.dart
@@ -0,0 +1,24 @@
+import 'package:get/get_utils/get_utils.dart';
+
+abstract class AppValidators {
+  static String? validateEmail(String? value) {
+    final trimmedValue = value?.trim() ?? "";
+    if (trimmedValue.isEmpty) {
+      return 'Please enter an email address';
+    }
+    if (value == null || !GetUtils.isEmail(value)) {
+      return 'Please enter a valid email address';
+    }
+    return null;
+  }
+  static String? validatePassword(String? value) {
+    final trimmedValue = value?.trim() ?? "";
+    if (trimmedValue.isEmpty) {
+      return 'Please enter a password';
+    }
+    if (value!.length < 6) {
+      return 'must be at least 6 characters long';
+    }
+    return null;
+  }
+}
